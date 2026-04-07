6a34da9a34f21dc1b3323e7d050ed805ff93bf7c
ravi31186 <ravi31186@gmail.com>
2026-04-07 18:21:37 -0500

    added backend and integrated with FE code

--- server/app.py
+++ server/app.py
@@ -83,6 +83,7 @@ def predict():
             "error": str(e)
         }), 400
 
-
+import os
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=True, host="0.0.0.0", port=port)
